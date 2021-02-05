2d1ffa203800d976f555e9e602ff3f8483fb4d00
Dynamite <mdejaz4450@gmail.com>
2021-02-05 14:44:14 +0530

    First

--- Inroduction.cpp
+++ Inroduction.cpp
@@ -1,7 +1,26 @@
 #include<iostream>
 using namespace std;
 
+class Employee{
+public:
+    string Name;
+    string Company;
+    int Age;
+
+    void IntroduceYourself(string name, string company, int age)
+    {
+    cout<<"Hello my name is "<<name<<" and my age is "<<age<<" and i worked in a "<<company;
+    }
+
+}; 
 int main(){
-    cout<<"WELCOME TO THE OBJECT ORIENTED PROGRAMMING IN C++";
+    Employee employee1; 
+    employee1.Name = "Mr.spkie";
+    employee1.Company = "YouTube";
+    employee1.Age = 21;
+
+   // employee1.IntroduceYourself(employee1.Name, employee1.Company, employee1.Age);
+
+ 
     return 0;
 }
